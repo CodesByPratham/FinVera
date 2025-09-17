676fbe38df230afa44e875815ead7591a1cd7781
Pratham Rathod <prathamr.dev@gmail.com>
2025-09-17 22:30:17 +0530

    New changes pushed

--- src/main/java/com/pratham/finvera/FinveraApplication.java
+++ src/main/java/com/pratham/finvera/FinveraApplication.java
@@ -1,14 +1,10 @@
 package com.pratham.finvera;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -22,12 +18,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableCaching
 public class FinveraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinveraApplication.class, args);
-	}
- 
-	@Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
+    public static void main(String[] args) {
+        SpringApplication.run(FinveraApplication.class, args);
     }
 }
